00a4c4df5f2c2845a2b298753911b6ff286cda89
ShevninEA <ev.shevnin@yandex.ru>
2023-03-03 17:47:16 +0300

    Готово

--- ConnectionString.cs
+++ ConnectionString.cs
@@ -8,5 +8,9 @@ namespace Lesson_4
 {
     public class ConnectionString
     {
+        public string Host { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string DatabaseName { get; set; }
     }
 }
